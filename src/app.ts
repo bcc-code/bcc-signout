ea9cf9997ddf3a1ce321c3469279e8d46921ed39
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-06-18 14:18:52 +0200

    moved to router

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,7 @@ import * as expressWinston from 'express-winston';
 import cors from 'cors';
 import debug from 'debug';
 import dotenv from 'dotenv';
-import { CommonRoutesConfig } from './common/routes.config';
-import { UserSessionsRoutes } from './userSession/userSessions.routes.config';
-
+import { userSessionsRouter } from './userSession/userSessions.router';
 
 const dotenvResult = dotenv.config();
 if (dotenvResult.error) {
@@ -17,7 +15,7 @@ if (dotenvResult.error) {
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = process.env.PORT ?? 3000;
-const routes: Array<CommonRoutesConfig> = [];
+
 const debugLog: debug.IDebugger = debug('app');
 
 const loggerOptions: expressWinston.LoggerOptions = {
@@ -40,17 +38,15 @@ app.use(express.json())
 //app.use(cors);
 app.use(expressWinston.logger(loggerOptions));
 
-routes.push(new UserSessionsRoutes(app));
 
 const runningMessage = `Server running at http://localhost:${port}`;
+app.use('/usersessions', userSessionsRouter)
+
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send(runningMessage)
 });
 
 server.listen(port, () => {
-    routes.forEach((route: CommonRoutesConfig) => {
-        debugLog(`Routes configured for ${route.getName()}`);
-    });
     console.log(runningMessage);
 });
 
---
+++ src/userSession/userSessions.router.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import UserSessionsController from './controllers/userSessions.controller';
+
+const router = express.Router()
+
+router.route("/")
+    .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`List of active user sessions`);
+    })
+
+router.route("/:userId")
+    .get(UserSessionsController.getAllUserSessions)
+    .post(UserSessionsController.storeUserSession)
+
+export { router as userSessionsRouter };
