0058b849718da2cb6d548be189d0a6c298076ac4
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-06-18 14:49:01 +0200

    fetch apps ids

--- src/logout/services/logout.service.ts
+++ src/logout/services/logout.service.ts
@@ -4,7 +4,8 @@ const log: debug.IDebugger = debug('redis-client');
 
 class LogoutService {
     async performFederatedLogOut(userId: string) {
-        return `logout performed for user ${userId}`
+        const userSessions = await redisClient.smembersAsync(userId)
+        return `logout performed for user ${userId} on these aps: ${userSessions}`
     }
 }
 
