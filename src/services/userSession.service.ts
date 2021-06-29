3a7e6f16cd61da9026a3bae2f1fe0079de010430
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-06-29 14:47:21 +0200

    handle refresh

--- src/services/userSession.service.ts
+++ src/services/userSession.service.ts
@@ -11,6 +11,8 @@ class UserSessionService implements SessionService {
         const response = await redisClient.saddAsync(userId, appId)
         if(response === 1) {
             return { message: "OK", userId: userId, appId: appId }
+        } else if(response === 0) {
+            return { message: "Refreshed", userId: userId, appId: appId }
         } else {
             throw new Error(`Unexpected response from redis instance: ${response}`)
         }
