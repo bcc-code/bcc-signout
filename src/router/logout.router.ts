338cf51286d27bc636e8fed7dcdaf2b8292e6979
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-07-04 15:18:07 +0200

    added validation to logout endpoint

--- src/router/logout.router.ts
+++ src/router/logout.router.ts
@@ -1,8 +1,13 @@
 import express from 'express'
 import logoutController from '../controllers/logout.controller'
+import { validate } from '../middlewares/validate.middleware'
+import { logoutRequestValidationRules } from '../validators/logoutRequest.validator'
 
 const router = express.Router()
 
-router.route('/:userId').post(logoutController.triggerLogout)
+router.route('/:userId').post(
+    logoutRequestValidationRules(),
+    validate,
+    logoutController.triggerLogout)
 
 export { router as logoutRouter }
