44a17c87478630316dc5955c9c1f9956eb09f164
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-07-13 18:18:10 +0200

    fixed clientConfiguration service

--- src/services/clientConfiguration.service.ts
+++ src/services/clientConfiguration.service.ts
@@ -3,7 +3,7 @@ import { ClientConfiguration } from '../interfaces/clientConfiguration.interface
 var fs = require('fs')
 
 class ClientConfigurationService {
-    readClientConfig(clientId: string): string {
+    readClientConfig(clientId: string): string[]|string {
         const appsData = JSON.parse(
             fs.readFileSync('./data/clientConfig.json', 'utf8')
         ) as ClientConfiguration[]
