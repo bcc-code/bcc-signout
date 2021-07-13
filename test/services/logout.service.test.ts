ab6ed9b9465497ac4ac94cf3ee8844e62c5aca25
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-07-13 19:12:47 +0200

    added stub of test

---
+++ test/services/logout.service.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+import userSessionService from '../../src/services/userSession.service'
+import { UserSessionMetadata } from '../../src/interfaces/userSessionMetadata.interface'
+const redisTestInterface = require('../setup/redis-test-interface')
+
+describe('UserService works properly', function () {
+    const testUserSession: UserSessionMetadata = {
+        clientId: 'CLIENTID',
+        userId: 'USERID',
+        sessionId: 'SESSIONID',
+        state: 'STATE',
+    }
+
+    beforeEach(async () => {
+        redisTestInterface.flushData()
+    })
+
+    it('stores user session', async function () {
+        var result = await userSessionService.storeUserSession(testUserSession)
+
+        expect(result).to.be.an('object').that.has.property('message', 'OK')
+
+        var readOut = redisTestInterface.client.smembers(
+            'USERID::SESSIONID::CLIENTID',
+            function (err: Error, res: any) {
+                console.log(res)
+                expect(res).to.be.an('array')
+                res.forEach((element: string) => {
+                    expect(element).to.be.a('string')
+                    expect(element).to.contain('STATE')
+                })
+            }
+        )
+    })
+})
