98ce6690b6eae655bff4806f2fccf576a6dee64d
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-07-16 12:03:53 +0200

    controler updated

--- src/controllers/logout.controller.ts
+++ src/controllers/logout.controller.ts
@@ -6,19 +6,27 @@ const log: debug.IDebugger = debug('controller:logout')
 class LogoutController {
     async triggerLogout(req: express.Request, res: express.Response) {
         log(req.params)
-        const logoutActions = await LogoutService.performFederatedLogout({
-            userId: req.params.userId,
-            sessionId: req.body.sessionId,
-            state: req.body.state,
-        })
-        if (logoutActions.result === 'OK') {
-            res.status(200).json({ result: logoutActions })
-        } else {
-            res.status(500).json({
-                message:
-                    'Encountered internal error, request was not succesfull',
+        if(req.query.userId && req.query.sessionId) {
+            const logoutActions = await LogoutService.performFederatedLogout({
+                userId: req.query.userId.toString(),
+                sessionId: req.query.sessionId.toString(),
             })
+            if (logoutActions.result === 'OK') {
+                res.status(200).json({ result: logoutActions })
+            } else {
+                res.status(500).json({
+                    message:
+                        'Encountered internal error, request was not succesfull',
+                })
+            }
+        } else {
+            res.status(422).json({
+                message:
+                    'Query params are missing',
+            })
+
         }
+        
     }
 }
 
