9bbf9dbb9186e5f5c4dd223e37edee82433657f5
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-07-30 14:22:36 +0200

    add scripts

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 echo "Running integration tests."
-docker exec workspace_signout_1 npm test-e2e || exit 1
+docker exec workspace_signout_1 npm test || exit 1
 if [ "$?" != 0 ]; then
-    echo "E2E tests failed."
+    echo "Integration tests failed."
     exit 1
 fi
 echo "Test ran sucessfully."
