ba712a6c0066a23482f20d7935e8396771f0f76d
mikolaj.kubista <mikolaj.kubista@gmail.com>
2021-07-30 14:31:50 +0200

    fixed script call

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "Running integration tests."
-docker exec workspace_signout_1 npm run test-e2e || exit 1
+docker exec workspace_signout_1 npm test || exit 1
 if [ "$?" != 0 ]; then
     echo "Integration tests failed."
     exit 1
